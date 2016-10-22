4e754ff4fe6b867b4a64999494a1c4a888d27a5c
Peter Yang <me@peteryang.io>
2016-10-22 00:41:23 -0700

    Missing slash on og:Url

--- short.php
+++ short.php
@@ -17,7 +17,7 @@ if (!$result->num_rows == 0){
     //Google analytics tracker
     include_once("../common/analyticstracking.php");
     echo "<meta property='og:title' content='$title'>";
-    echo "<meta property='og:url' content='https://${_SERVER['HTTP_HOST']}$short'>";
+    echo "<meta property='og:url' content='https://${_SERVER['HTTP_HOST']}/$short'>";
     echo "<meta property='og:image' content='$image'>";
     echo "<meta property='og:description' content='$description'>";
     echo "<script>location = '$link'</script>";
