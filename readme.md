5fa7f4b83c240fbbb3e17ec4749b25ca58a095f2
Peter Yang <me@peteryang.io>
2016-10-22 17:37:23 -0700

    Documented short

--- readme.md
+++ readme.md
@@ -1,6 +1,7 @@
 # Link Shortener with Open Graph Mirroring
 ## Functionality
 This link shortener is intended to be installed on a server with a database. When used, a custom short link will be created and a dummy page will be created containg Open Graph data scraped from the original link. After the dummy page prints the meta:og tags which is enough for any OG reader to scrape data, it will then instantly redirect to the full link. For YouTube Links, this will scrape a separate OG format which Facebook cannot detect and thus will show up as a large preview image as opposed to the forced small YouTube icons on posts
+
 ## Upcoming Features
 * Easier method of copying newly generated short link
 * More intutive way of managing preexisting short links. Will prompt user to either overwrite with new full link or cancel
@@ -11,4 +12,4 @@ This link shortener is intended to be installed on a server with a database. Whe
 
 ## Known Bugs
 * Currently does not provide image or video height/width so Facebook cannot asyncronously download a preview
-https://en.wikipedia.org/wiki/Category:Lists_of_insects
+* https://en.wikipedia.org/wiki/Category:Lists_of_insects
--- short.php
+++ short.php
@@ -1,12 +1,20 @@
 <?php
 require 'serverconnect.php';
-
+/**
+    @return String
+        The video ID of a YouTube video
+*/
 function parseVideoIdFromLink($link){
     $linkParsed = parse_url($link, PHP_URL_QUERY);
     $arr = explode("v=", $linkParsed);
     return $arr[1];
 }
 
+/**
+    .htaccess says
+        RewriteRule ^(.*)$ /short.php?val=$1
+    val is used as the main key for matching to full link and data in database
+*/
 $short = $mysql->escape_string($_GET["val"]);
 
 $query = "SELECT title, image, description, link FROM linkTable WHERE short='$short'";
@@ -17,6 +25,8 @@ if (!$result->num_rows == 0){
     $image = htmlspecialchars($row['image'], ENT_QUOTES);
     $description = htmlspecialchars($row['description'], ENT_QUOTES);
     $link = htmlspecialchars($row['link'], ENT_QUOTES);
+    
+    // If it is a YouTube link, we create an og:video tag so Facebook can embed the player
     $isYouTube = false;
     if (strpos(parse_url($link, PHP_URL_HOST), 'youtube')){
         $videoId = parseVideoIdFromLink($link);
@@ -31,6 +41,7 @@ if (!$result->num_rows == 0){
     echo "<meta property='og:image' content='$image'>";
     echo "<meta property='og:description' content='$description'>";
     if ($isYouTube){
+        // Autplay is necessary otherwise Facebook will require clicking two play buttons
         echo "<meta property='og:video' content='https://youtube.com/v/$videoId&autoplay=1'";
     }
     echo "<script>location = '$link'</script>";
